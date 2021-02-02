84681f48afcd2d7f83efb8dce33d738f65b30d13
Matheus Henrique <matheus.181137@gmail.com>
2021-02-02 14:15:05 -0400

    chore: change js to ts/tsx file extension

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
+import * as React from 'react'
+import type { AppProps } from 'next/app'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+const MyApp: React.FunctionComponent<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
 
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+const Home: React.FunctionComponent = () => {
+  return (
+    <div>
+      <p>Retorno</p>
+    </div>
+  )
+}
+
+export default Home

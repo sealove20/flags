76d4fb8544c11c14f9d4d3e15bc4b76fec5654ae
Matheus Henrique <matheus.181137@gmail.com>
2021-02-02 23:29:39 -0400

    feat: create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+const Header: React.FunctionComponent = () => {
+  return (
+    <header className="w-screen h-24 px-8 shadow-md flex justify-between items-center">
+      <p>Where in the world?</p>
+      <p>Dark Mode</p>
+    </header>
+  )
+}
+
+export default Header

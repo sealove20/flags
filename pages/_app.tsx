a0eba66fc134fc51fc85dee54e92de07300c0da2
Matheus Henrique <matheus.181137@gmail.com>
2021-02-10 00:12:19 -0400

    chore: config dark theme toggle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
 
 import '../styles/globals.css'
 
 const MyApp: React.FunctionComponent<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,11 @@
+import * as React from 'react'
+import ThemeToggle from '../ThemeToggle'
+
 const Header: React.FunctionComponent = () => {
   return (
-    <header className="w-screen h-24 px-8 shadow-md flex justify-between items-center">
-      <p>Where in the world?</p>
-      <p>Dark Mode</p>
+    <header className="w-screen h-24 px-8 shadow-md flex justify-between items-center bg-white dark:bg-gray-600">
+      <p className="text-gray-800 dark:text-white">Where in the world?</p>
+      <ThemeToggle />
     </header>
   )
 }

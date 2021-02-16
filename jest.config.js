972f0440eab4d6f0f1fd58f531a69e172ebb29c2
Matheus Henrique <matheus.181137@gmail.com>
2021-02-15 21:02:38 -0400

    chore: config separated jest.config for dev/build and jest environments

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,18 @@
 module.exports = {
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json'
+    }
+  },
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}'
   ],
   coverageDirectory: 'coverage',
   setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   testEnvironment: 'jsdom',
+  preset: 'ts-jest',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
   }

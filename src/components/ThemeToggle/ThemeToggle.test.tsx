ae05b79b97f0dddd87aeff452d0293933ebe17c2
Matheus Henrique <matheus.181137@gmail.com>
2021-02-24 20:48:20 -0400

    test: ensure change between themes when toggleButton is clicked

--- src/components/ThemeToggle/ThemeToggle.test.tsx
+++ src/components/ThemeToggle/ThemeToggle.test.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render, fireEvent } from '../../../utils/tests/helper'
+import { useTheme } from 'next-themes'
 
 import ThemeToggle from '@/components/ThemeToggle'
 
-test('Should render ThemeToggle component', () => {
-  const { getByRole } = render(<ThemeToggle />)
+const ThemeSpy: React.FunctionComponent = () => {
+  const { theme } = useTheme()
+  return <span data-testid="theme-spy">{theme}</span>
+}
 
-  console.log(fireEvent.click(getByRole('button', { name: 'Toggle Dark Mode' })))
+test('Should render ThemeToggle component', () => {
+  const { getByTestId, getByRole } = render(
+    <>
+      <ThemeToggle />
+      <ThemeSpy />
+    </>,
+    { theme: 'dark' }
+  )
+  const spy = getByTestId('theme-spy')
+  const button = getByRole('button', { name: 'Toggle Dark Mode' })
+
+  fireEvent.click(button)
+  fireEvent.click(button)
+  expect(spy).toHaveTextContent('light')
 })

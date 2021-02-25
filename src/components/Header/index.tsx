00d2704d5f9a9b7e31254636717b0b3e42105c85
Matheus Henrique <matheus.181137@gmail.com>
2021-02-24 22:58:29 -0400

    chore: add default theme colors

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,8 @@ import ThemeToggle from '@/components/ThemeToggle'
 
 const Header: React.FunctionComponent = () => {
   return (
-    <header className="w-screen h-24 px-8 shadow-md flex justify-between items-center bg-white dark:bg-gray-600">
-      <p className="text-gray-800 dark:text-white">Where in the world?</p>
+    <header className="w-screen h-24 px-8 shadow-md flex justify-between items-center bg-light-background dark:bg-dark-background">
+      <p className="text-light-text dark:text-white">Where in the world?</p>
       <ThemeToggle />
     </header>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,27 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'dark-background': 'hsl(209, 23%, 22%)',
+        'dark-element': 'hsl(207, 26%, 17%)',
+        'light-input': 'hsl(0, 0%, 52%)',
+        'light-background': 'hsl(0, 0%, 98%)'
+      },
+      textColor: {
+        'light-text': 'hsl(200, 15%, 8%)'
+      }
+    }
   },
   variants: {
     extend: {}
   },
   plugins: []
 }
+
+// - Dark Blue (Dark Mode Elements): hsl(209, 23%, 22%)
+// - Very Dark Blue (Dark Mode Background): hsl(207, 26%, 17%)
+// - Very Dark Blue (Light Mode Text): hsl(200, 15%, 8%)
+// - Dark Gray (Light Mode Input): hsl(0, 0%, 52%)
+// - Very Light Gray (Light Mode Background): hsl(0, 0%, 98%)
+// - White (Dark Mode Text & Light Mode Elements): hsl(0, 0%, 100%)

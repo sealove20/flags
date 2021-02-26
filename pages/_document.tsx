1a64931429e9ce912c1ee082a9c8cd39b98937d0
Matheus Henrique <matheus.181137@gmail.com>
2021-02-25 23:15:58 -0400

    feat: add dark mode to body background

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&family=Oi&display=swap" rel="stylesheet" />
         </Head>
-        <body>
+        <body className="bg-light-background dark:bg-dark-background">
           <Main />
           <NextScript />
         </body>
